0d87e15c304f77428486955b9c263b59bb05b079
Leonard Bongard <leo.bongard@gmx.de>
2021-07-08 17:56:12 +0200

    remove prints

--- fisher_annotator.py
+++ fisher_annotator.py
@@ -197,20 +197,28 @@ class Parser(DisfluencyTagger):
             subbatch_sentences = [[(dummy_tag, word) for word in sentence] for sentence in subbatch_sentences]
             predicted, _ = self.parser.parse_batch(subbatch_sentences)
             del _
-            all_predicted.extend([p.convert() for p in predicted])
+            all_predicted.extend([p.convert() for p in predicted]) 
         
         parse_trees, df_labels = [], []
+        print([p.convert() for p in predicted])
+        print("allp", all_predicted)
         for tree in all_predicted:      
+            print("tree", tree)
             linear_tree = tree.linearize()
 
             parse_trees.append(linear_tree)
             if self.disfluency:
                 tokens = linear_tree.split()
+                print("tokens", tokens)
+                print("linear tree", linear_tree)
                 # disfluencies are dominated by EDITED nodes in parse trees
                 if "EDITED" not in linear_tree: 
                     df_labels.append(self.fluent(tokens))
+                
                 elif not remove_disfluency_words_bool:
                     df_labels.append(self.disfluent(tokens))
+                print(df_labels)
+                print(parse_trees)
                     
         return parse_trees, df_labels
 
@@ -264,6 +272,7 @@ class Annotate(Parser):
         # Loop over transcription files
         trans_file =  self.input_path
         segments = self.read_transcription(trans_file) 
+        print("segments:", segments)
         # Loop over cleaned/pre-proceesed transcripts         
         doc = [segment for segment in segments if segment]    
         parse_trees, df_labels = self.run_parser(doc, remove_df_words)
@@ -317,15 +326,18 @@ class Annotate(Parser):
 
         return
 
-    def read_transcription(self, trans_file):
+    def read_transcription(self, trans_file, skip_token=False):  # Skip token isnerted by leonard
+        skip = 3 if skip_token else 0
         with codecs.open(trans_file, "r", "utf-8") as fp:
             for line in fp:
                 if line.startswith("#") or len(line) <= 1:
                     continue                
                 tokens = line.split() 
-                yield self.validate_transcription(
-                    " ".join(tokens[3:])
-                    )           
+
+                yield " ".join(tokens[skip:])
+                #yield self.validate_transcription(
+                 #   " ".join(tokens[skip:])
+                 #   )           
 
     @staticmethod
     def validate_transcription(label):
