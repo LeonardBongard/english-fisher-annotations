f0e1ed0bc0f9bf34d934af300e89403d5429f74b
Leonard Bongard <leonard.bongard@bongard.de>
2021-09-01 10:28:38 +0200

    remove unnecessary parameters and code

--- fisher_annotator.py
+++ fisher_annotator.py
@@ -15,6 +15,7 @@ https://www.aclweb.org/anthology/2020.acl-main.346.pdf
 import os
 
 import sys
+
 #sys.path.append("/home/stud-leonardbongard/Bachelor_Arbeit/ba-leonard-bongard/models/submodules/english-fisher-annotations")
 sys.path.append(os.path.join(os.getcwd(), "models", "submodules",
                              "english-fisher-annotations"))  # Should add main repo dir to paths
@@ -84,81 +85,6 @@ class DisfluencyTagger:
         return " ".join(list(map(lambda t: " ".join(t), tags)))
 
 
-"""
-class Parser_OLD(DisfluencyTagger):  # new parser system to load model only ones  
-    "" "
-    Loads the pre-trained parser model to find silver parse trees     
-   
-    Returns:
-        Parsed and disfluency labelled transcripts
-    "" "
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-    def torch_load(self):
-        if parse_nk.use_cuda:
-            return torch.load(
-                self.model
-                )
-        else:
-            return torch.load(
-                self.model, 
-                map_location=lambda storage, 
-                location: storage,
-                )
-
-    def run_parser(self, input_sentences, vocab_path, remove_disfluency_words_bool=False):
-        eval_batch_size = 1
-        print("Loading model from {}...".format(self.model))  # TODO load only once insetead every time
-        assert self.model.endswith(".pt"), "Only pytorch savefiles supported"
-        info = self.torch_load()  # TODO load only once insetead every time
-
-        assert "hparams" in info["spec"], "Older savefiles not supported"
-
-        info["spec"]["hparams"]["bert_model"] = vocab_path  # TODO load only once insetead every time
-
-        "" "
-        info["spec"]:
-        {'hparams': {'attention_dropout': 0.2, 'bert_do_lower_case': True, 'bert_model': './model/bert-base-uncased.tar.gz', 'bert_transliterate': '', 'char_lstm_input_dropout': 0.2, 'clip_grad_norm': 0, 'd_char_emb': 32, 'd_ff': 2048, 'd_kv': 64, 'd_label_hidden': 250, 'd_model': 1024, 'd_tag_hidden': 250, 'elmo_dropout': 0.5, 'embedding_dropout': 0.0, 'learning_rate': 5e-05, 'learning_rate_warmup_steps': 160, 'max_consecutive_decays': 3, 'max_len_dev': 0, 'max_len_train': 0, 'morpho_emb_dropout': 0.2, 'num_heads': 8, 'num_layers': 2, 'num_layers_position_only': 0, 'partitioned': True, 'predict_tags': False, 'relu_dropout': 0.1, 'residual_dropout': 0.2, 'sentence_max_len': 300, 'silver_weight': 4, 'step_decay': True, 'step_decay_factor': 0.5, 'step_decay_patience': 5, 'tag_emb_dropout': 0.2, 'tag_loss_scale': 5.0, 'timing_dropout': 0.0, 'use_bert': True, 'use_bert_only': False, 'use_chars_lstm': False, 'use_elmo': False, 'use_tags': False, 'use_words': False, 'word_emb_dropout': 0.4}, 'char_vocab': <vocabulary.Vocabulary object at 0x7f56a3d144d0>, 'label_vocab': <vocabulary.Vocabulary object at 0x7f56a3c99e90>, 'word_vocab': <vocabulary.Vocabulary object at 0x7f56a3cb1490>, 'tag_vocab': <vocabulary.Vocabulary object at 0x7f56a3314c90>}
-        "" "
-        parser = parse_nk.NKChartParser.from_spec(
-            info["spec"], 
-            info["state_dict"],
-
-        )  # TODO load only once insetead every time
-
-        print("Parsing sentences...")
-        sentences = [sentence.split() for sentence in input_sentences]
-        # Tags are not available when parsing from raw text, so use a dummy tag
-        if "UNK" in parser.tag_vocab.indices:
-            dummy_tag = "UNK"
-        else:
-            dummy_tag = parser.tag_vocab.value(0)
-        
-        all_predicted = []
-        for start_index in range(0, len(sentences), eval_batch_size):
-            subbatch_sentences = sentences[start_index:start_index+eval_batch_size]
-            subbatch_sentences = [[(dummy_tag, word) for word in sentence] for sentence in subbatch_sentences]
-            predicted, _ = parser.parse_batch(subbatch_sentences)
-            del _
-            all_predicted.extend([p.convert() for p in predicted])
-        
-        parse_trees, df_labels = [], []
-        for tree in all_predicted:      
-            linear_tree = tree.linearize()
-
-            parse_trees.append(linear_tree)
-            if self.disfluency:
-                tokens = linear_tree.split()
-                # disfluencies are dominated by EDITED nodes in parse trees
-                if "EDITED" not in linear_tree: 
-                    df_labels.append(self.fluent(tokens))
-                elif not remove_disfluency_words_bool:
-                    df_labels.append(self.disfluent(tokens))
-                    
-        return parse_trees, df_labels
-"""
-
 class Parser(DisfluencyTagger):
     """
     Loads the pre-trained parser model to find silver parse trees     
@@ -210,12 +136,17 @@ class Parser(DisfluencyTagger):
             all_predicted.extend([p.convert() for p in predicted]) 
         
         parse_trees, df_labels = [], []
+        print([p.convert() for p in predicted])
+        print("allp", all_predicted)
         for tree in all_predicted:      
+            print("tree", tree)
             linear_tree = tree.linearize()
 
             parse_trees.append(linear_tree)
             if self.disfluency:
                 tokens = linear_tree.split()
+                print("tokens", tokens)
+                print("linear tree", linear_tree)
                 # disfluencies are dominated by EDITED nodes in parse trees
                 if "EDITED" not in linear_tree: 
                     df_labels.append(self.fluent(tokens, return_list))
@@ -257,20 +188,6 @@ class Annotate(Parser):
 
 
     def setup(self): 
-        # all_2004 = self.parse_sentences(            
-        #     trans_data=os.path.join(
-        #         "LDC2004T19", 
-        #         "fe_03_p1_tran", 
-        #         "data", 
-        #         "trans",
-        #         ),
-        #     parsed_data="fisher-2004-annotations"
-        # )
-
-        # all_2005 = self.parse_sentences(
-
-        #     parsed_data="fisher-2005-annotations"
-        # )
         return self.parse_sentences_for_one_file(self.remove_df)
 
 
@@ -284,52 +201,28 @@ class Annotate(Parser):
         #print("doc", doc)
         parse_trees, df_labels = self.run_parser(doc, remove_df_words, return_list)
         #print(parse_trees, df_labels)
-        df_labels = self.remove_labels(df_labels, remove_df_words)
-        #print("df_labels", df_labels)
+        df_labels = self.remove_labels(df_labels)
 
-        # if self.disfluency:
-        #     print("Write into file:", self.output_path)
-        #     with open(self.output_path, "w") as output_file:
-        #         output_file.write("\n".join(df_labels))
-
-        # return "\n ".join(df_labels)  # TODO may reenable after disfluency prep
         if return_list:
             return df_labels
         return " ".join(df_labels)
 
     def parse_sentences_for_one_sentence(self, doc,remove_df_words, return_list=False): 
         # Loop over transcription files
-        #trans_file =  self.input_path
-        #segments = self.read_transcription(trans_file) 
-        #print("segments:", segments)
-        # Loop over cleaned/pre-proceesed transcripts      
-        # print("#################")
-        # print(segments)
-
-        # trans_file =  self.input_path
-        # segments = self.read_transcription(trans_file) 
-        # print(segments)
-        # print("++++++++++++++++++++++++")
         remove_df_words = self.remove_df   
         # doc = [segment for segment in segments if segment]    
         parse_trees, df_labels = self.run_parser(doc, remove_df_words, return_list)
-        df_labels = self.remove_labels(df_labels, remove_df_words)
+        print("dflabel1", df_labels)
+        df_labels = self.remove_labels(df_labels)
 
-
-        # if self.disfluency:
-        #     print("Write into file:", self.output_path)
-        #     with open(self.output_path, "w") as output_file:
-        #         output_file.write("\n".join(df_labels))
         if return_list:
             return df_labels
         return "\n".join(df_labels)
 
-    def remove_labels(self, df_labels, remove_df_words):
+    def remove_labels(self, df_labels):
         """
         Method to remove all disfluency annotated labels
         """
-        if not remove_df_words:
-            return df_labels
         return [label.replace(" _", "") for label in df_labels]
 
     def parse_sentences(self, trans_data, parsed_data):
@@ -339,8 +232,11 @@ class Annotate(Parser):
         if not os.path.exists(output_dir):
             os.makedirs(output_dir)   
         # Loop over transcription files
+        print(input_dir)
         for root, dirnames, filenames in os.walk(input_dir):
+            print(root, dirnames, filenames )
             for filename in fnmatch.filter(filenames, "*.txt"):
+                print(filename)
                 trans_file = os.path.join(root, filename)
                 segments = self.read_transcription(trans_file) 
                 # Loop over cleaned/pre-proceesed transcripts         
@@ -364,22 +260,19 @@ class Annotate(Parser):
 
         return
 
-    def read_transcription(self, trans_file, skip_token=False):  # Skip token inserted by leonard
+    def read_transcription(self, trans_file, skip_token=False):  # Skip token isnerted by leonard
         skip = 3 if skip_token else 0
         with codecs.open(trans_file, "r", "utf-8") as fp:
             for line in fp:
                 if line.startswith("#") or len(line) <= 1:
                     continue     
-                #line = line.replace("\n", " ")          
 
                 tokens = line.split() 
 
                 #print(tokens)
 
                 yield " ".join(tokens[skip:])
-                #yield self.validate_transcription(
-                 #   " ".join(tokens[skip:])
-                 #   )           
+       
 
     @staticmethod
     def validate_transcription(label):
